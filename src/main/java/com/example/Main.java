066a47a6af7cd009fdcb4bdd27b4bf45758c01de
kim <kim23362611@gmail.com>
2021-08-04 17:28:57 +0800

    db connection

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2002-2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example;
 
 import com.zaxxer.hikari.HikariConfig;
@@ -24,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -35,16 +21,18 @@ import java.util.ArrayList;
 import java.util.Map;
 
 @Controller
-@SpringBootApplication
+@SpringBootApplication  //說明這是一個Spring Boot
 public class Main {
 
   @Value("${spring.datasource.url}")
   private String dbUrl;
-
+  @Value("${test}")
+  private String test;
   @Autowired
   private DataSource dataSource;
-
+  
   public static void main(String[] args) throws Exception {
+	  //啟動Spring
     SpringApplication.run(Main.class, args);
   }
 
@@ -54,7 +42,11 @@ public class Main {
   }
   
   @RequestMapping("/js-map")
-  String jsmap() {
+  String jsmap(
+		  @RequestParam(name="name", required = false, defaultValue = "")String name,Model model  
+		  ) {
+	  model.addAttribute("name",name);
+	  model.addAttribute("account",1000);
     return "js-map";
   }
 
